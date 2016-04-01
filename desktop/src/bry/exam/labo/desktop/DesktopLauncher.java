776d5fc3f414884af658c1a79acc27846b6ccf6d
Bryan Medrano <Bryan Medrano>
2016-04-01 16:22:32 -0600

    first commit

---
+++ desktop/src/bry/exam/labo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package bry.exam.labo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import bry.exam.labo.koalio;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new koalio(), config);
+	}
+}
